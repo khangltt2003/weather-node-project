dfcb4c46e5367fc0f4d9d96ecf243938ae9c5e12
khangltt2003 <khangltt2003@gmail.com>
2023-06-16 21:13:19 -0500

    .

--- app.js
+++ app.js
@@ -9,23 +9,28 @@ app.use(bodyParser.urlencoded({extended:true})); //used to parse data from form
 const APIKey  = `41fd7e85e504fe468bb922c1f39500cc`;
 
 app.get('/', (req, res) =>{
-    res.sendFile(__dirname+"/index.html")
+    res.sendFile(__dirname+"/index.html") //send html file to web
 })
 
-app.post("/",(req,res)=>{
+app.post("/",(req,res)=>{  //req = data from form
 
     console.log(req.body.cityName);
 
     let URL = `https://api.openweathermap.org/data/2.5/weather?q=${req.body.cityName}&appid=${APIKey}&units=metric`
+
+    //get api using https
     https.get(URL,(response)=>{
         response.on("data", (data)=>{
+            console.log(data); // data sent back in form in hex buffer
+            let weatherData = JSON.parse(data); // use JSON.parse to convert data to json
             console.log(JSON.parse(data));
-            let weatherData = JSON.parse(data);
-
+            
+            //get data from api
             let temp = weatherData.main.temp;
             let weatherDescription = weatherData.weather[0].description;
             let icon = weatherData.weather[0].icon;
 
+            //post
             res.write(`<body style = "height: 100vh; background-color: black; display:flex; flex-direction: column; align-items: center; justify-content: center"></body>`)
             res.write(`<h1 style = "color:orange"> The temperature at ${weatherData.name} is ${temp} degree Celsius. </h1>`);
             res.write(`<h1 style = "color:blue"> The weather is currently ${weatherDescription}. </h1>`)
